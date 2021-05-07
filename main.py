44b235dc58fb55667e09a885859bffc830d33921
Elliot-Wadge <75158330+Elliot-Wadge@users.noreply.github.com>
2021-05-06 18:59:59 -0700

    committing the kv and main folder

---
+++ main.py
@@ -0,0 +1,18 @@
+import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+
+
+
+
+
+
+class TimeShamer(App):
+    def build(self):
+        return BoxLayout()
+
+
+
+if __name__ == '__main__':
+    TimeShamer().run()
