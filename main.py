15a1efbe2abf66fd4e972b01a3f6978f672f5c2c
Elliot-Wadge <75158330+Elliot-Wadge@users.noreply.github.com>
2021-05-06 19:11:23 -0700

    implemented the screen manager

--- main.py
+++ main.py
@@ -2,15 +2,26 @@ import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager,  Screen
+from kivy.lang import Builder
+
+class Window1(Screen):
+    pass
 
 
+class Window2(Screen):
+    pass
 
 
+class WindowManager(ScreenManager):
+    pass
 
+
+kv = Builder.load_file('TimeShamer.kv')
 
 class TimeShamer(App):
     def build(self):
-        return BoxLayout()
+        return kv
 
 
 
