6fa2e81aa5e457fc8922ff70a38c6bc858eb4a79
Elliot-Wadge <75158330+Elliot-Wadge@users.noreply.github.com>
2021-05-07 19:25:24 -0700

    reading in our text files, seperate kv

---
+++ Scroll.py
@@ -0,0 +1,60 @@
+import kivy
+from kivy.app import App
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.label import Label
+from random import shuffle
+from kivy.uix.gridlayout import GridLayout
+
+
+
+
+class Scroll(ScrollView):
+
+
+    #takes the filenames needed to access and the number of string needed
+    #from each file and whatever kwargs Scrollview takes
+    def __init__(self,filenames,n, **kwargs):
+        super(Scroll,self).__init__(**kwargs)
+        store = ''
+        #might want to take multiple filenames for the different lengths
+        for i,filename in enumerate(filenames):
+
+            #open file
+            f = open(filename,'r')
+            #skip the first two lines
+
+            f.readline()
+            f.readline()
+            #read all of the lines
+
+            lst = f.readlines()
+            #shuffle the lst, O(n) but lets me cut out one for loop
+            shuffle(lst)
+
+            #slice the necessary amount
+            lst = lst[:n[i]]
+            print(lst)
+            #iterate through the remaining
+            for string in lst:
+                for index,char in enumerate(string):
+                    if char == ',':
+                        store += string[:index] + '\n'
+
+            f.close()
+        self.ids.lbl.text = store
+        print(store)
+
+
+
+
+
+class Myapp(App):
+    def build(self):
+
+        return Scroll(['motivators/short'],[100],size_hint = (1,None), do_scroll_y = True)
+
+
+
+
+if __name__ == '__main__':
+    Myapp().run()
